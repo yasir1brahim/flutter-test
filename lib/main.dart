31546b8451d64d377bab35bdf39a8bdb75aaeb37
Yasir Ibrahim <yibrahim@knysys.com>
2022-12-23 21:12:36 +0500

    Show photos list and its detail page.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,10 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+
+import 'models/Photo.dart';
+
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +17,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Photos App',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -48,68 +54,109 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+    return MaterialApp(
+      title: 'Photos',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Photos'),
+        ),
+        body: Center(
+          child: FutureBuilder <List<Photo>>(
+            future: fetchData(),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                List<Photo>? data = snapshot.data;
+                return
+                /*
+                  ListView.builder(
+                      itemCount: data?.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return Container(
+                          height: 75,
+                          color: Colors.white,
+                          child: Center(child: Text(data![index].title),
+                          ),);
+                      }
+                  );
+                */
+                ListView(
+                  children: get_list_tiles(data!),
+                );
+              } else if (snapshot.hasError) {
+                return Text("${snapshot.error}");
+              }
+              // By default show a loading spinner.
+              return CircularProgressIndicator();
+            },
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  List<ListTile> get_list_tiles(List<Photo> photos){
+    List<ListTile> list_tile = [];
+    for (var i = 0; i < photos.length; i++) {
+      list_tile.add(ListTile(
+        onTap: (){
+      Navigator.push(
+      context,
+      MaterialPageRoute(
+      builder: (context) => PhotoDetail(photo: photos[i]),
+      ),
+      );
+      },
+        leading: Image.network(photos[i].thumbnailUrl),
+        title: Text(
+          photos[i].title,
+          textScaleFactor: 1.5,
+        ),
+      ));
+    }
+
+    return list_tile;
+  }
+
+  Future <List<Photo>> fetchData() async {
+    var url = Uri.parse('https://jsonplaceholder.typicode.com/photos');
+    final response =
+    await http.get(url);
+    if (response.statusCode == 200) {
+      List jsonResponse = json.decode(response.body);
+      return jsonResponse.map((data) => new Photo.fromJson(data)).toList();
+    } else {
+      throw Exception('Unexpected error occured!');
+    }
+  }
+}
+
+class PhotoDetail extends StatelessWidget {
+  // In the constructor, require a Todo.
+  const PhotoDetail({super.key, required this.photo});
+
+  // Declare a field that holds the Todo.
+  final Photo photo;
+
+  @override
+  Widget build(BuildContext context) {
+    // Use the Todo to create the UI.
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Photo Details"),
+      ),
+      body: Column(
+
+        children: [
+          Image.network(photo.thumbnailUrl),
+          Text(
+           photo.title,
+            style: TextStyle(fontSize: 20.0),
+          )
+        ],
+      )
     );
   }
 }
